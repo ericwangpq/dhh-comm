7af8bcbd51bef819359c3b2030e93a1696f81221
Eric Wang <ewpq@outlook.com>
2025-02-21 10:13:50 +0800

    initialized an automated .app to execute the app without using command line tools

---
+++ run_streamlit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /opt/anaconda3/etc/profile.d/conda.sh
+conda init bash
+conda activate dhh-comm-env
+
+cd /Users/ericwang/git/dhh-comm
+
+/opt/anaconda3/envs/dhh-comm-env/bin/streamlit run app.py --server.runOnSave=true
